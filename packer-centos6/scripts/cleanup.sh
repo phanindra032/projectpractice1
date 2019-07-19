3635f93f33299b727d4713540dbc662a8a6d2b14
root <root@localhost.localdomain>
2019-07-19 15:28:09 -0400

    this is master file

---
+++ packer-centos6/scripts/cleanup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Remove Ansible and its dependencies.
+yum -C -y remove ansible python-crypto python-paramiko python-markupsafe python-httplib2 sshpass python-setuptools python-crypto2 python-simplejson python-pyasn1 python-keyczar libyaml PyYAML python-six python-babel python-jinja2
+
+# Delete old kernels
+package-cleanup -C -y --oldkernels --count=1
+
+# Clean yum cache
+echo "Cleaning up extra files"
+rm -rf /var/cache/yum/*
+rm -rf /usr/share/man/*
+rm -rf /usr/share/info/*
+rm -rf /usr/share/doc/*
+
+# Zero out the rest of the free space using dd, then delete the written file.
+dd if=/dev/zero of=/EMPTY bs=1M
+rm -f /EMPTY
+
+# Add `sync` so Packer doesn't quit too early, before the large file is deleted.
+sync
